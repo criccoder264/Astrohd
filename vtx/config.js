4e46bab17a93ffa620e9d5b4dd9457aedc39d3c0
criccoder264 <shantanux264@gmail.com>
2025-01-22 16:25:01 +0530

    Update config.js

--- vtx/config.js
+++ vtx/config.js
@@ -1,5 +1,5 @@
 // URL of the PHP endpoint
-const phpEndpoint = "https://criccoder264.pages.dev/videoConfigs.php";
+const phpEndpoint = "https://criccoder264.pages.dev/vtx/videoConfigs.php";
 
 // Get the 'id' parameter from the URL
 const urlParams = new URLSearchParams(window.location.search);
